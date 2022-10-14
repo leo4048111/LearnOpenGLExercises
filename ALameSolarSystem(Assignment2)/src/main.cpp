bf34252c5a007df81dde5a711cc93f8522ca3d8b
Potato5K <74029782+leo4048111@users.noreply.github.com>
2022-10-14 13:31:01 +0800

    Just some random codes for fun
    Just some random codesJust some random codes for fun

--- ALameSolarSystem(Assignment2)/src/main.cpp
+++ ALameSolarSystem(Assignment2)/src/main.cpp
@@ -60,7 +60,7 @@ int main()
 	io.WantSaveIniSettings = false;
 
 	// attribs of 8 planets + sun + moon
-	float sunMass = 333400.f, mercuryMass = 1.f, venusMass = 1.f, earthMass = 4000.f, moonMass = 1.f, marsMass = 1.f, jupiterMass = 1.f, saturnMass = 1.f, uranusMass = 1.f, neptuneMass = 1.f, plutoMass = 1.f;
+	float sunMass = 333400.f, mercuryMass = 1.f, venusMass = 1.f, earthMass = 4000.f, moonMass = 1.f, marsMass = 1.f, jupiterMass = 3000.f, saturnMass = 3000.f, uranusMass = 1.f, neptuneMass = 1.f, plutoMass = 1.f;
 	float mercuryE = 0.6f, venusE = 0.65f, earthE = 0.7f, moonE = 0.7f, marsE = 0.72f, jupiterE = 0.75f, saturnE = 0.79f, uranusE = 0.81f, neptuneE = 0.83f, plutoE = 0.85f;
 	float mercuryFD = 30.f, venusFD = 60.f, earthFD = 100.f, moonFD = 40.f, marsFD = 140.f, jupiterFD = 170.f, saturnFD = 200.f, uranusFD = 230.f, neptuneFD = 260.f, plutoFD = 300.f;
 	glm::vec3 sunScale = { 1.0f, 1.0f, 1.0f }, mercuryScale = { 0.2f, 0.2f, 0.2f }, venusScale = { 0.3f, 0.3f, 0.3f }, earthScale = { 0.5f, 0.5f, 0.5f }, moonScale = { 0.1f, 0.1f, 0.1f };
@@ -70,19 +70,23 @@ int main()
 	glm::vec4 marsColor = { 0.73f, 0.33f, 0.23, 1.0f }, jupiterColor = { 0.57f, 0.40f, 0.25, 1.0f }, saturnColor = { 0.89f, 0.71f, 0.49f, 1.0f }, uranusColor = { 0.16f,0.75f, 0.93f, 1.0f }, neptuneColor = { 0.16f,0.75f, 0.93f, 1.0f };
 	glm::vec4 plutoColor = { 0.46f, 0.67f, 0.71f, 1.0f };
 
-	//init world
+	// init world
 	g_world->init(50, 50, 20.f);
 	g_world->addPlanet("Sun", "Sun", 1.0f, 0.0f, shader, sunMass, { 0.0f, 0.0f, 0.0f }, sunScale, sunColor);
 	g_world->addPlanet("Mercury", "Sun", mercuryE, mercuryFD, shader, mercuryMass, { mercuryFD, 0.0f, 0.0f }, mercuryScale, mercuryColor);
 	g_world->addPlanet("Venus", "Sun", venusE, venusFD, shader, venusMass, { venusFD, 0.0f, 0.0f }, venusScale, venusColor);
 	g_world->addPlanet("Earth", "Sun", earthE, earthFD, shader, earthMass, { earthFD, 0.0f, 0.0f }, earthScale, earthColor);
-	g_world->addPlanet("Moon", "Earth", moonE, moonFD, shader, moonMass, { moonFD, 0.0f, 0.0f }, moonScale, moonColor);
 	g_world->addPlanet("Mars", "Sun", marsE, marsFD, shader, marsMass, { marsFD, 0.0f, 0.0f }, marsScale, marsColor);
 	g_world->addPlanet("Jupiter", "Sun", jupiterE, jupiterFD, shader, jupiterMass, { jupiterFD, 0.0f, 0.0f }, jupiterScale, jupiterColor);
 	g_world->addPlanet("Saturn", "Sun", saturnE, saturnFD, shader, saturnMass, { saturnFD, 0.0f, 0.0f }, saturnScale, saturnColor);
 	g_world->addPlanet("Uranus", "Sun", uranusE, uranusFD, shader, uranusMass, { uranusFD, 0.0f, 0.0f }, uranusScale, uranusColor);
 	g_world->addPlanet("Neptune", "Sun", neptuneE, neptuneFD, shader, neptuneMass, { neptuneFD, 0.0f, 0.0f }, neptuneScale, neptuneColor);
 	g_world->addPlanet("Pluto", "Sun", plutoE, plutoFD, shader, plutoMass, { plutoFD, 0.0f, 0.0f }, plutoScale, plutoColor);
+
+	// some satellites just for fun
+	g_world->addPlanet("Moon", "Earth", moonE, moonFD, shader, moonMass, { moonFD, 0.0f, 0.0f }, moonScale, moonColor);
+	g_world->addPlanet("Ganymede", "Jupiter", 0.6, 50.f, shader, 1.0f, { moonFD, 0.0f, 0.0f }, moonScale, { 0.21f, 0.22 , 0.17 ,1.0f });
+	g_world->addPlanet("Titan", "Saturn", 0.6, 50.f, shader, 1.0f, { moonFD, 0.0f, 0.0f }, moonScale, saturnColor);
 
 	shader->uniform3fv("u_lightColor", {1.0f, 1.0f, 1.0f});
 	shader->uniform3fv("u_lightPos", { 0.0f, 0.0f, 0.0f });
